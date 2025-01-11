f76350aa1eca5e3da27d2611cab97beee15bfbe2
nurlanlan <nurahmad004@gmail.com>
2025-01-11 15:45:51 +0400

    first commit

---
+++ language-learning-service/language-learning-service/src/test/java/com/coeus/language_learning_service/LanguageLearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coeus.language_learning_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
