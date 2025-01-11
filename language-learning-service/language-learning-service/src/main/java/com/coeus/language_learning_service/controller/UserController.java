ca1f22be0541cf45bdf655d2f5ca7a82f59db1f2
nurlanlan <nurahmad004@gmail.com>
2025-01-11 21:13:57 +0400

    controller, service updated

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/controller/UserController.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@NoArgsConstructor
+
 public class UserController {
     UserService userService;
     @PostMapping("/register")
