f121edd3530a6d659be9957606548da8fc105f68
nurlanlan <nurahmad004@gmail.com>
2025-01-12 17:09:05 +0400

    last version

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/security/SecurityConfig.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
