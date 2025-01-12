fe4a0441af5bb9d1ae70b935bd10814fa89fd9e4
nurlanlan <nurahmad004@gmail.com>
2025-01-12 17:03:51 +0400

    security config updated

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/security/SecurityConfig.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // CSRF müdafiəsini deaktiv edirik (lazım olsa aktiv edin)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/register").permitAll() // Açıq endpointlər
-                        .anyRequest().permitAll() // Qalan bütün sorğular üçün autentifikasiya tələb olunur
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic autentifikasiyanı deaktiv edirik
                 .formLogin(formLogin -> formLogin.disable()); // Default form-login deaktivdir
