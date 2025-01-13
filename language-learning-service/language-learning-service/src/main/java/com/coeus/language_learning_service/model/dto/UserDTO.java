1557424122951d052736371f691b847db3b3050b
nurlanlan <nurahmad004@gmail.com>
2025-01-13 17:39:07 +0400

    dto contructors added

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/model/dto/UserDTO.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/model/dto/UserDTO.java
@@ -1,8 +1,12 @@
 package com.coeus.language_learning_service.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private String name;
     private String surname;
