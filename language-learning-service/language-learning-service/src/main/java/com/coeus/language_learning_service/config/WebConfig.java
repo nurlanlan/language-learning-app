cdaf94d7b5bb6102b3958a43c344b3ee4716ddfe
nurlanlan <nurahmad004@gmail.com>
2025-01-13 18:09:58 +0400

    configs dropped

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/config/WebConfig.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/config/WebConfig.java
@@ -1,14 +1,14 @@
-package com.coeus.language_learning_service.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:9090");
-    }
-}
+//package com.coeus.language_learning_service.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("http://localhost:9090");
+//    }
+//}
