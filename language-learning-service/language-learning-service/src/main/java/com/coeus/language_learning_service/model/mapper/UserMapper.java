b06ff43122504cb04d52312ecd5b5a394e009a1c
nurlanlan <nurahmad004@gmail.com>
2025-01-13 17:47:27 +0400

    mapper executed

--- language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/model/mapper/UserMapper.java
+++ language-learning-service/language-learning-service/src/main/java/com/coeus/language_learning_service/model/mapper/UserMapper.java
@@ -1,11 +1,11 @@
-package com.coeus.language_learning_service.model.mapper;
-
-import com.coeus.language_learning_service.domain.entity.User;
-import com.coeus.language_learning_service.model.dto.UserDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    UserDTO toDTO(User user);
-    User toEntity(UserDTO userDTO);
-}
+//package com.coeus.language_learning_service.model.mapper;
+//
+//import com.coeus.language_learning_service.domain.entity.User;
+//import com.coeus.language_learning_service.model.dto.UserDTO;
+//import org.mapstruct.Mapper;
+//
+//@Mapper(componentModel = "spring")
+//public interface UserMapper {
+//    UserDTO toDTO(User user);
+//    User toEntity(UserDTO userDTO);
+//}
